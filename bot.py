80aafaec639ff8d1a2f4aab5330fa75a688bf03f
samadafatsamadov-cpu <samad.afat.samadov@gmail.com>
2026-06-26 03:06:38 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ import json
 import os
 from datetime import datetime
 
-BOT_TOKEN = 8908020561:AAHA52gPEwUXeO0a0kskG7NtxIaY6jYrCc8
-GEMINI_API_KEY = AQ.Ab8RN6JAPbEVkD8u-mXjWTZdjUR7famIZmOknGZUo3EYT6olpg 
+BOT_TOKEN = "8908020561:AAHA52gPEwUXeO0a0kskG7NtxIaY6jYrCc8"
+GEMINI_API_KEY = "AQ.Ab8RN6JAPbEVkD8u-mXjWTZdjUR7famIZmOknGZUo3EYT6olpg"
 # İstifadəçiləri saxlayan fayl
 USERS_FILE = "users.json"
 
